2500f32a676a48806f9ab09f6927fff717d253cd
EricThomson <thomson.eric@gmail.com>
2025-06-18 16:33:51 -0400

    update pyproject and added __init__ with version

---
+++ src/sightsprite/__init__.py
@@ -0,0 +1,11 @@
+"""
+sight sprite: machine vision with voice alerts 
+"""
+__version__ = "0.1.0"
+
+from .capture import get_snapshot
+from .capture import get_snapshots
+from .capture import get_video
+from .capture import show_test_image
+from .capture import show_test_video
+

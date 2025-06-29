2ca237725d21b61d128cad8e6fd2ed8736186952
EricThomson <thomson.eric@gmail.com>
2025-06-29 10:46:37 -0400

    added capture functions to init

--- src/sightsprite/__init__.py
+++ src/sightsprite/__init__.py
@@ -8,11 +8,15 @@ from .capture import get_snapshots
 from .capture import capture_video
 from .capture import show_test_image
 from .capture import show_test_video
+from .training import ImageLabeler
+from .training import sort_images_by_label
 
 
 __all__ = ["get_snapshot",
            "get_snapshots",
            "capture_video",
            "show_test_image",
-           "show_test_video"]
+           "show_test_video"
+           "ImageLabeler",
+           "sort_images_by_label"]
 
